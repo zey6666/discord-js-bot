5441e15e428f0a5934858c3511c96ea44a54f043
zey6666 <adharvng298@gmail.com>
2024-07-18 14:14:00 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["936202587600994335"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/M5g2Nx3M", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
     DEFAULT_PREFIX: "!", // Default prefix for the bot
